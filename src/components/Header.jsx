ef3bd246fac47fcae70cde5153a881a5f696e7b7
Noxolo <wendyngcobo98@gmail.com>
2024-05-28 17:43:18 +0200

    create header component and add component to App

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="header--image"/>
+      <h1 className="header--title">Meme Generator</h1>
+      <p className="header--project">React Course - Project 3</p>
+    </div>
+  )
+}
+
+export default Header;
