7918bf7dda9bd2ddf8ab2dcdbe11c7027f197994
Noxolo <wendyngcobo98@gmail.com>
2024-05-28 18:54:53 +0200

    add classes to elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,12 +2,12 @@
 
 const Form = () => {
   return (
-    <form>
+    <form className="form">
         <label htmlFor="top-text">Top text</label>
-        <input type="text" id="top-text"></input>
+        <input type="text" id="top-text" className="form--input"></input>
         <label htmlFor="bottom-text">Bottom text</label>
-        <input type="text" id="bottom-text"></input>
-        <input type="submit"></input>
+        <input type="text" id="bottom-text" className="form--input"></input>
+        <input type="submit" className="form--button"></input>
     </form>
   )
 }
