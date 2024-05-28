1db3b0afc49606252baca53f97b95489e4bcbefc
Noxolo <wendyngcobo98@gmail.com>
2024-05-28 18:18:31 +0200

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+
+//Headeder component
+// Uses classes from
+
 const Header = () => {
   return (
     <div className="header">
-      <img className="header--image"/>
+      <img className="header--image" src="./src/img/logo.jpg" alt="logo"/>
       <h1 className="header--title">Meme Generator</h1>
       <p className="header--project">React Course - Project 3</p>
     </div>
