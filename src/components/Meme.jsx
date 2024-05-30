f3a76b09851095081ff135485d6a19cb3b14e70c
Noxolo <wendyngcobo98@gmail.com>
2024-05-30 16:12:43 +0200

    add value attributes from the meme texts

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -39,11 +39,17 @@ function Meme() {
 
       <div className="form">
           <label>Top text
-            <input type="text" id="top-text" className="form--input" placeholder="Shut up" name="topText"></input>
+            <input type="text" id="top-text" 
+            className="form--input" placeholder="Shut up" 
+            name="topText"
+            value={meme.topText}></input>
           </label>
           
           <label>Bottom text
-            <input type="text" id="bottom-text" className="form--input"  placeholder="and take my money" name="bottomText"></input>
+            <input type="text" id="bottom-text" 
+            className="form--input"  placeholder="and take my money" 
+            name="bottomText"
+            value={meme.bottomText}></input>
           </label>
           
           <button type="submit" className="form--button" onClick={getMemeImage}> Get a new meme image 🖼️</button>
